5febbf89ed077ef53277c1fdff31b34546248ea8
Olivier Gorzalka <olivier@amphibee.fr>
2022-01-10 13:41:59 +0100

    Added the ability to create dynamic view calls (useful for overriding template parts)

--- src/View/ViewServiceProvider.php
+++ src/View/ViewServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Themosis\View;
 
+use Illuminate\View\Engines\CompilerEngine;
 use Illuminate\View\Factory;
+use Illuminate\View\View;
 use Illuminate\View\ViewServiceProvider as IlluminateViewServiceProvider;
 use Themosis\View\Engines\Twig;
 use Themosis\View\Extensions\WordPress;
@@ -21,6 +23,56 @@ class ViewServiceProvider extends IlluminateViewServiceProvider
         $this->registerTwigLoader();
         $this->registerTwigEnvironment();
         $this->registerTwigEngine();
+
+        $this->registerBladeMacros();
+    }
+
+    /**
+     * Register Blade View Macros
+     *
+     * @return void
+     */
+    public function registerBladeMacros()
+    {
+        $app = $this->app;
+
+        /**
+         * Get the compiled path of the view
+         *
+         * @return string
+         */
+        View::macro('getCompiled', function () {
+            /** @var string $file path to file */
+            $file = $this->getPath();
+
+            /** @var \Illuminate\Contracts\View\Engine $engine */
+            $engine = $this->getEngine();
+
+            return ($engine instanceof CompilerEngine)
+                ? $engine->getCompiler()->getCompiledPath($file)
+                : $file;
+        });
+
+        /**
+         * Creates a loader for the view to be called later
+         *
+         * @return string
+         */
+        View::macro('makeLoader', function () use ($app) {
+            $view = $this->getName();
+            $path = $this->getPath();
+            $id = md5($this->getCompiled());
+            $compiled_path = $app['config']['view.compiled'];
+
+            $content = "<?= \\view('{$view}', \$data ?? get_defined_vars())->render(); ?>"
+                . "\n<?php /**PATH {$path} ENDPATH**/ ?>";
+
+            if (!file_exists($loader = "{$compiled_path}/{$id}-loader.php")) {
+                file_put_contents($loader, $content);
+            }
+
+            return $loader;
+        });
     }
 
     /**
