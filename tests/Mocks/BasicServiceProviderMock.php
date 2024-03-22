cc79497cce02744d69c3002be3382c524e761012
Steve Junker <steve.junker@axelspringer.com>
2024-03-22 12:08:53 +0100

    MSP-3488: added .editorconfig, phpcs

--- tests/Mocks/BasicServiceProviderMock.php
+++ tests/Mocks/BasicServiceProviderMock.php
@@ -4,12 +4,17 @@ namespace Themosis\Tests\Mocks;
 
 
 use Illuminate\Support\ServiceProvider;
+use Themosis\Tests\Application;
 
 class BasicServiceProviderMock extends ServiceProvider
 {
+    use Application;
+
     public function __construct()
     {
+        parent::__construct($this->getApplication());
     }
+
     public function register()
     {
     }
