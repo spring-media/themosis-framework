83ce8a3b9953d5d8e666c412e30e5a157ba76d80
Steve Junker <steve.junker@axelspringer.com>
2025-07-01 10:20:09 +0200

    MSP-3844: deferrable fix (from putrefaction/framework)

--- src/Core/Providers/ArtisanServiceProvider.php
+++ src/Core/Providers/ArtisanServiceProvider.php
@@ -100,13 +100,6 @@ use Themosis\Core\Console\WidgetMakeCommand;
 class ArtisanServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     /**
-     * Defer the loading of the provider.
-     *
-     * @var bool
-     */
-    protected $defer = true;
-
-    /**
      * Commands to register.
      *
      * @var array
