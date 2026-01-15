8c433276801f6f740d2e7ac951ed63aca41875f2
Steve Junker <steve.junker@axelspringer.com>
2026-01-15 15:23:36 +0100

    fix(dependencies): add override for preact, set version to 12.0.3 #[MSP-5802]

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -26,7 +26,7 @@ class Application extends \Illuminate\Foundation\Application implements
     /**
      * Themosis framework version.
      */
-    public const THEMOSIS_VERSION = '12.0.2';
+    public const THEMOSIS_VERSION = '12.0.3';
 
     /**
      * Application textdomain.
